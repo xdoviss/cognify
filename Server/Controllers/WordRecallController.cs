37155aa43a5dc4a1ed996f8c990f67440a728c2b
Dovydas <58664779+xdoviss@users.noreply.github.com>
2024-09-18 11:39:29 +0300

    Add game select menu, implement word recall basic functionality (#3)
    Co-authored-by: Dovydas Podinskas <Dovydas.Podinskas@bentley.com>

---
+++ Server/Controllers/WordRecallController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cognify.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WordRecallController : ControllerBase
+{
+    private static readonly List<string> SeenWords = new();
+    private static readonly HttpClient HttpClient = new();
+
+    [HttpGet]
+    public async Task<ActionResult<string>> Get()
+    {
+        string word = await GetNewWord();
+
+        while (SeenWords.Contains(word))
+            word = await GetNewWord();
+
+        SeenWords.Add(word);
+
+        return word;
+    }
+
+    private static async Task<string> GetNewWord()
+    {
+        var response = await HttpClient.GetStringAsync("https://random-word-api.herokuapp.com/word");
+        var words = System.Text.Json.JsonSerializer.Deserialize<List<string>>(response);
+        var word = words[0];
+
+        return word;
+    }
+}

3df928e8f66ecac16330f9c366dd8f1bdaeb02ab
FireHunterLM <146267068+FireHunterLM@users.noreply.github.com>
2024-11-27 13:48:25 +0200

    Exceptions and automatic migration (#28)

--- Server/Program.cs
+++ Server/Program.cs
@@ -22,6 +22,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Apply pending migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate(); // Applies any pending migrations
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
